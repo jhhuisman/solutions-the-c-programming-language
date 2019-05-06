bd74ad1f48c4c3f21d4c833719bf4ece7b8eafb1
Jaap Huisman <jaap.huisman89@gmail.com>
2019-05-06 11:57:11 +0000

    1-21

---
+++ chapter1/1-21/entab.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+#define TABBLANKS 4
+
+int entab(int blankcount);
+
+int main()
+{
+	int c, blankcount;
+
+	blankcount = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ')
+			++blankcount;
+		else {
+			entab(blankcount);
+			putchar(c);
+			blankcount = 0;
+		}
+	}
+
+	return 0;
+}
+
+int entab(int blankcount)
+{
+	while (blankcount >= 4) {
+		putchar('\t');
+		blankcount = blankcount - 4;
+	}
+
+	for (blankcount; blankcount > 0; --blankcount)
+		putchar(' ');
+
+	return 0;
+}
