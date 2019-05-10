2250bf14b767e9c9badc0ef560a79f8ca5d3f1bf
Jaap Huisman <jaap.huisman89@gmail.com>
2019-05-10 11:07:34 +0000

    2-5 improved

--- chapter2/2-5/any.c
+++ chapter2/2-5/any.c
@@ -26,18 +26,14 @@ int main()
 
 int any(char s1[], char s2[])
 {
-	int i, j, location;
+	int i, j;
 
-	location = -1;
-
-	for (i = 0; s1[i] != '\0' && location == -1; i++) {
+	for (i = 0; s1[i] != '\0'; i++) {
 		for (j = 0; s2[j] != '\0'; j++) {
-			if (s1[i] == s2[j]) {
-				location = i;
-				break;
-			}
+			if (s1[i] == s2[j])
+				return i;
 		}
 	}
 
-	return location;
+	return -1;
 }
