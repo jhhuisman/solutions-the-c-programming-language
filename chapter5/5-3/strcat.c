9d585af83989d5bb075145bbb51c67a28ee9b7c3
Jaap Huisman <jaap.huisman89@gmail.com>
2019-07-29 11:27:05 +0100

    strcat pointer version

---
+++ chapter5/5-3/strcat.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void strcatp(char *s, char *t);
+void strcat_kr(char s[], char t[]);
+
+int main()
+{
+	char hello[11];
+	char bye[4];
+
+	hello[0] = 'h';
+	hello[1] = 'e';
+	hello[2] = 'l';
+	hello[3] = 'l';
+	hello[4] = 'o';
+	hello[5] = '\0';
+
+	bye[0] = 'b';
+	bye[1] = 'y';
+	bye[2] = 'e';
+	bye[3] = '\0';
+
+	strcatp(hello, bye);
+
+	printf("%s\n", hello);
+
+	return 0;
+}
+
+void strcatp(char *s, char *t)
+{
+	while (*s) /* Find end of s */
+		s++;
+
+	while (*s++ = *t++)
+		;
+}
+
+/* strcat: concatenate t to the end of s; s must be big enough */
+void strcat_kr(char s[], char t[])
+{
+	int i, j;
+
+	i = j = 0;
+
+	/* Find end of s */
+	while (s[i] != '\0')
+		i++;
+
+	while ((s[i++] = t[j++]) != '\0')
+		;
+}
