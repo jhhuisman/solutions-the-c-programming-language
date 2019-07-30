9c8ebcdcba89d8e431db9ac88e0c1d1cbc5dfd77
Jaap Huisman <jaap.huisman89@gmail.com>
2019-07-30 10:16:44 +0100

    5-6 getline

--- chapter5/5-6/getline/getline.c
+++ chapter5/5-6/getline/getline.c
@@ -1,8 +1,31 @@
+/* Inspired by C Answer Book page 104 */
 #include <stdio.h>
+#define MAXLINE 1000
+
+int my_getline(char *s, int lim);
 
 int main()
 {
-	printf("## TEST\n");
+	char line[MAXLINE];
+
+	while (my_getline(line, MAXLINE) > 0)
+		printf("%s", line);
 
 	return 0;
+}
+
+int my_getline(char *s, int lim)
+{
+	int c;
+	char *firstp = s;
+
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		*s++ = c;
+
+	if (c == '\n')
+		*s++ = c;
+
+	*s = '\0';
+
+	return s - firstp;
 }
