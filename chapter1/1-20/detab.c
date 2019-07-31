44255c80732fd8fc3b2298f900f4d8b1bffef874
Jaap Huisman <jaap.huisman89@gmail.com>
2019-07-31 15:02:10 +0100

    1-20 detab improved

--- chapter1/1-20/detab.c
+++ chapter1/1-20/detab.c
@@ -1,28 +1,40 @@
+/* Inspired by C Answer Book page 31 */
 #include <stdio.h>
 
 #define MAXLINE 1000
-#define TABBLANKS 4
+#define TABBLANKS 8
 
 void detab();
 
 int main()
 {
 	int c;
+	int pos = 1; /* position in line */
 
 	while ((c = getchar()) != EOF) {
 		if (c == '\t')
-			detab();
-		else
+			detab(&pos);
+		else if (c == '\n') {
 			putchar(c);
+			pos = 1;
+		}
+		else {
+			putchar(c);
+			pos++;
+		}
 	}
 
 	return 0;
 }
 
-void detab()
+void detab(int *pos_p)
 {
-	int i;
+	int nspaces; /* number of spaces needed */
+	
+	nspaces = TABBLANKS - (*pos_p % TABBLANKS) + 1;
 
-	for (i = 0; i <= TABBLANKS; ++i)
+	while (nspaces-- > 0) {
 		putchar(' ');
+		(*pos_p)++;
+	}
 }
