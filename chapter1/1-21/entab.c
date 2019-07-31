b11d1acee6cf15ac2f27a113dab14d47b2f14752
Jaap Huisman <jaap.huisman89@gmail.com>
2019-07-31 16:05:52 +0100

    1-21 entab improve

--- chapter1/1-21/entab.c
+++ chapter1/1-21/entab.c
@@ -1,36 +1,56 @@
 #include <stdio.h>
 
 #define MAXLINE 1000
-#define TABBLANKS 4
+#define TABBLANKS 8
 
-void entab(int blankcount);
+void entab(int blankcount, int *pos_p);
+int pos_after_tab(int *pos_p);
 
 int main()
 {
-	int c, blankcount;
+	int c, blankcount, pos;
 
 	blankcount = 0;
+	pos = 1;
 
 	while ((c = getchar()) != EOF) {
 		if (c == ' ')
 			++blankcount;
 		else {
-			entab(blankcount);
+			entab(blankcount, &pos);
 			putchar(c);
 			blankcount = 0;
+
+			if (c == '\n')
+				pos = 1;
+			else
+				pos++;
 		}
 	}
 
 	return 0;
 }
 
-void entab(int blankcount)
+void entab(int blankcount, int *pos_p)
 {
-	while (blankcount >= 4) {
+	int endpos; /* end position after all tabs and spaces */
+
+	endpos = *pos_p + blankcount;
+
+	while (endpos >= pos_after_tab(pos_p)) {
 		putchar('\t');
-		blankcount = blankcount - 4;
+		*pos_p += pos_after_tab(pos_p);
 	}
 
-	for (blankcount; blankcount > 0; --blankcount)
+	while ((*pos_p)++ < endpos)
 		putchar(' ');
+}
+
+int pos_after_tab(int *pos_p)
+{
+	int n_tabspaces; /* number of spaces needed for next tab */
+
+	n_tabspaces = TABBLANKS - (*pos_p % TABBLANKS) + 1;
+
+	return *pos_p + n_tabspaces;
 }
